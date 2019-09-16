14e7a3438ba395a0cee60aac632f10f5f50a2a7d
Max Nagl <max@na.gl>
2019-09-16 14:59:22 +0200

    commit

---
+++ javaBugDummyLib/src/main/java/de/siebn/javaBug/DroidBug.java
@@ -0,0 +1,9 @@
+package de.siebn.javaBug;
+
+public class DroidBug extends JavaBug {
+    public static void addAndroidPlugins() {
+    }
+
+    public static void setApplication(final Object app) {
+    }
+}
---
+++ javaBugDummyLib/src/main/java/de/siebn/javaBug/JavaBug.java
@@ -0,0 +1,36 @@
+package de.siebn.javaBug;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class JavaBug {
+    public static void init(int port) {
+    }
+
+    public static boolean isStarted() {
+        return false;
+    }
+
+    public static void start() {
+
+    }
+
+    public static void stop() {
+    }
+
+    public static void addRootObject(String name, Object object) {
+
+    }
+
+    public static void addPlugin(Object plugin) {
+
+    }
+
+    public static ArrayList<String> getIPAddresses(boolean includeLoopback) {
+        return new ArrayList<>();
+    }
+
+    public static void addFileRoot(String name, File root) {
+
+    }
+}
