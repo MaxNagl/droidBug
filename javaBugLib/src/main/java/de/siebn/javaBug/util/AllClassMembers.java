109a4c3c6bd53b985198da5660639a10f5d001ec
Max Nagl <max@na.gl>
2019-10-28 10:41:17 +0100

    commit

--- javaBugLib/src/main/java/de/siebn/javaBug/util/AllClassMembers.java
+++ javaBugLib/src/main/java/de/siebn/javaBug/util/AllClassMembers.java
@@ -16,7 +16,7 @@ public class AllClassMembers {
 
     public final ArrayList<Field> fields = new ArrayList<>();
     public final ArrayList<Method> methods = new ArrayList<>();
-    public final Map<String, POJO> pojos = new TreeMap();
+    public final Map<String, POJO> pojos = new TreeMap<>();
     public final ArrayList<BugProperty> properties = new ArrayList<>();
 
     public class POJO {
@@ -25,7 +25,7 @@ public class AllClassMembers {
     }
 
     private AllClassMembers(Class<?> clazz) {
-        addAllMembers(clazz);
+        addAllMembers(clazz, new HashSet<String>());
         Collections.sort(fields, new Comparator<Field>() { public int compare(Field o1, Field o2) { return o1.getName().compareTo(o2.getName());}});
         Collections.sort(methods, new Comparator<Method>() {@Override public int compare(Method o1, Method o2) {return o1.getName().compareTo(o2.getName());}});
         for (Field field: fields) properties.add(new BugFieldProperty(field));
@@ -34,16 +34,12 @@ public class AllClassMembers {
         }
     }
 
-    private void addAllMembers(Class<?> clazz) {
-        for (Field f : clazz.getDeclaredFields()) {
-            f.setAccessible(true);
-            fields.add(f);
-        }
-        m: for (Method m : clazz.getDeclaredMethods()) {
-            for (Method om : methods) {
-                if (om.getName().equals(m.getName()) && om.getReturnType().equals(m.getReturnType()) && Arrays.equals(m.getParameterTypes(), om.getParameterTypes())) continue m;
-            }
-            m.setAccessible(true);
+    private void addAllMembers(Class<?> clazz, Set<String> added) {
+        fields.addAll(Arrays.asList(clazz.getDeclaredFields()));
+        for (Method m : clazz.getDeclaredMethods()) {
+            StringBuilder identifierSb = new StringBuilder().append(m.getReturnType().getName()).append(" ").append(m.getName());
+            for (Class<?> p : m.getParameterTypes()) identifierSb.append(" ").append(p.getName());
+            if (!added.add(identifierSb.toString())) continue;
             methods.add(m);
             if (!clazz.equals(Object.class) && m.getName().length() > 3) {
                 String field = m.getName().substring(3);
@@ -59,7 +55,7 @@ public class AllClassMembers {
                 }
             }
         }
-        if (clazz.getSuperclass() != null) addAllMembers(clazz.getSuperclass());
+        if (clazz.getSuperclass() != null) addAllMembers(clazz.getSuperclass(), added);
     }
 
     public static AllClassMembers getForClass(Class<?> clazz) {
